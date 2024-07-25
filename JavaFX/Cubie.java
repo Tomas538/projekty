334c941ff3c73260c76f2b7588e48cd8a43759ba
Tomas538 <tmsb094@gmail.com>
2024-07-25 09:49:03 +0200

    Update Cubie.java

--- JavaFX/Cubie.java
+++ JavaFX/Cubie.java
@@ -1,36 +1,93 @@
-import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.TriangleMesh;
 import javafx.scene.transform.Translate;
 
 public class Cubie {
-
-    //vytvorenie kocky
-    private Box box;
-    private boolean farba = false;
-    private PhongMaterial material;
+    private MeshView meshView;
     private int x, y, z;
-    private final int len = 50;
+    private final int len = 40;
+    private int[] faceColors = {0, 1, 2, 3, 4, 5};
 
     public Cubie(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
-        box = new Box(len, len, len);
-        material = new PhongMaterial();
-        material.setDiffuseColor(Color.WHITE);
-        box.setMaterial(material);
-        box.getTransforms().add(new Translate(x * len, y * len, z * len));
+        meshView = createColoredCube();
+        meshView.getTransforms().add(new Translate(x * len, y * len, z * len));
     }
 
-    // Zmena farby na indexe
-    public void setFarba(boolean farba) {
-        this.farba = farba;
-        if (farba) {
-            material.setDiffuseColor(Color.RED);
-        } else {
-            material.setDiffuseColor(Color.WHITE);
+    private MeshView createColoredCube() {
+        TriangleMesh mesh = new TriangleMesh();
+
+        // rozloženie / tvar objektu
+        float h = len / 2.0f;
+        float[] points = {
+                -h, -h, -h,
+                h, -h, -h,
+                h, h, -h,
+                -h, h, -h,
+                -h, -h, h,
+                h, -h, h,
+                h, h, h,
+                -h, h, h
+        };
+        mesh.getPoints().addAll(points);
+
+        //nastavenie farieb
+        float[] texCoords = {
+                0.1f, 0.1f,   // front blue
+                0.8f, 0.2f,   // back green
+                0.7f, 0.5f,   // right orange
+                0.2f, 0.5f,   // left red
+                0.27f, 0.85f, // top white
+                0.7f, 0.85f,  // down yellow
+
+        };
+        mesh.getTexCoords().addAll(texCoords);
+
+        // označenie strán
+        int[] faces = {
+            0, faceColors[0], 2, faceColors[0], 1, faceColors[0],  0, faceColors[0], 3, faceColors[0], 2, faceColors[0],  // Front Blue
+            4, faceColors[1], 5, faceColors[1], 6, faceColors[1],  4, faceColors[1], 6, faceColors[1], 7, faceColors[1],  // Back Green
+            1, faceColors[2], 2, faceColors[2], 6, faceColors[2],  1, faceColors[2], 6, faceColors[2], 5, faceColors[2],  // Right Orange
+            3, faceColors[3], 0, faceColors[3], 4, faceColors[3],  3, faceColors[3], 4, faceColors[3], 7, faceColors[3],  // Left Red
+            0, faceColors[4], 1, faceColors[4], 5, faceColors[4],  0, faceColors[4], 5, faceColors[4], 4, faceColors[4],  // Top White
+            2, faceColors[5], 3, faceColors[5], 7, faceColors[5],  2, faceColors[5], 7, faceColors[5], 6, faceColors[5],  // Bottom Yellow
+            
+        };
+        mesh.getFaces().addAll(faces);
+
+        MeshView meshView = new MeshView(mesh);
+        PhongMaterial material = new PhongMaterial();
+        material.setDiffuseMap(createDiffuseMap());
+        meshView.setMaterial(material);
+
+        return meshView;
+    }
+    // vytvorenie farieb pre kocku
+    private Image createDiffuseMap() {
+        int width = 2;
+        int height = 3;
+        int[] colors = {
+          0xFF0000FF, // Blue
+          0xFF00FF00, // Green
+          0xFFFF0000, // Orange
+          0xFFFFA500, // Red
+          0xFFFFFFFF, // White
+          0xFFFFFF00,// Yellow
+        };
+        WritableImage image = new WritableImage(width, height);
+        PixelWriter writer = image.getPixelWriter();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                writer.setArgb(x, y, colors[y * width + x]);
+            }
         }
+        return image;
     }
 
     // Aktualizácia pozície
@@ -38,8 +95,33 @@ public class Cubie {
         this.x = x;
         this.y = y;
         this.z = z;
-        box.getTransforms().clear();
-        box.getTransforms().add(new Translate(x * len, y * len, z * len));
+        meshView.getTransforms().clear();
+        meshView.getTransforms().add(new Translate(x * len, y * len, z * len));
+        meshView.setMesh(createColoredCube().getMesh());
+    }
+
+    public void rotateFacesX() {
+        int temp = faceColors[4];
+        faceColors[4] = faceColors[1];
+        faceColors[1] = faceColors[5];
+        faceColors[5] = faceColors[0];
+        faceColors[0] = temp;
+    }
+
+    public void rotateFacesY() {
+        int temp = faceColors[2];
+        faceColors[2] = faceColors[1];
+        faceColors[1] = faceColors[3];
+        faceColors[3] = faceColors[0];
+        faceColors[0] = temp;
+    }
+
+    public void rotateFacesZ() {
+        int temp = faceColors[4];
+        faceColors[4] = faceColors[3];
+        faceColors[3] = faceColors[5];
+        faceColors[5] = faceColors[2];
+        faceColors[2] = temp;
     }
     // Gettery pre pozície
     public int getX() {
@@ -53,8 +135,7 @@ public class Cubie {
     public int getZ() {
         return z;
     }
-
-    public Box getBox() {
-        return box;
+    public MeshView getMeshView() {
+        return meshView;
     }
 }
