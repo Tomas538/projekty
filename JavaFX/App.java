aaacb71a671cd627f304e770ba25ec03c0979c22
Tomas538 <tmsb094@gmail.com>
2024-08-04 11:26:06 +0200

    Update App.java

--- JavaFX/App.java
+++ JavaFX/App.java
@@ -7,6 +7,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
@@ -21,12 +22,16 @@ public class App extends Application {
 
     private static final int DIM = 3;
     private Cubie[] cube = new Cubie[DIM * DIM * DIM];
+    TextArea movesTextArea = new TextArea();
+    private StringBuilder textHistory = new StringBuilder();
 
     @Override
     public void start(Stage primaryStage) {
         // vytvorenie scény
+        int width = 800;
+        int height = 550;
         SmartGroup group = new SmartGroup();
-        SubScene subScene = new SubScene(group, 800, 550, true, SceneAntialiasing.BALANCED);
+        SubScene subScene = new SubScene(group, width, height, true, SceneAntialiasing.BALANCED);
         subScene.setFill(Color.GREY);
         Camera camera = new PerspectiveCamera();
         subScene.setCamera(camera);
@@ -35,23 +40,29 @@ public class App extends Application {
         light.setLightOn(true);
         group.getChildren().add(light);
         
-        // vytvorenie a umiestnenie tlačidiel
+        // vytvorenie a umiestnenie tlačidiel a textového okna
         HBox rightRotation = new HBox(10);
         HBox leftRotation = new HBox(10);
-
         VBox buttons = new VBox(10);
         buttons.getChildren().addAll(rightRotation, leftRotation);
 
+        movesTextArea.setEditable(false);
+        movesTextArea.setPrefWidth(200);
+        movesTextArea.setPrefHeight(80);
+        movesTextArea.setWrapText(true);
+
         // pozicia kocky
-        group.translateXProperty().set(800 / 2);
-        group.translateYProperty().set(550 / 2);
+        group.translateXProperty().set(width / 2);
+        group.translateYProperty().set(height / 2);
         group.translateZProperty().set(0);
 
         // rozloženie a zobrazenie okna
         BorderPane root = new BorderPane();
         root.setCenter(subScene);
         root.setBottom(buttons);
-        Scene mainScene = new Scene(root, 800, 650);
+        root.setRight(movesTextArea);
+
+        Scene mainScene = new Scene(root, width+200, height+80);
 
         primaryStage.setTitle("3D Rubik's Cube");
         primaryStage.setScene(mainScene);
@@ -98,63 +109,76 @@ public class App extends Application {
         rightRotation.getChildren().addAll(buttonX1, buttonX2, buttonY1, buttonY2, buttonZ1, buttonZ2, bReset);
         leftRotation.getChildren().addAll(buttonx1, buttonx2, buttony1, buttony2, buttonz1, buttonz2, mix);
 
-        buttonX1.setOnAction(e -> turnX(-1));
-        buttonX2.setOnAction(e -> turnx(1));
-        buttonY1.setOnAction(e -> turnY(-1));
-        buttonY2.setOnAction(e -> turny(1));
-        buttonZ1.setOnAction(e -> turnZ(-1));
-        buttonZ2.setOnAction(e -> turnz(1));
-        bReset.setOnAction(e -> resetCube());
+        buttonX1.setOnAction(e -> {turnX(-1); updateMovesTextArea("L");});
+        buttonX2.setOnAction(e -> {turnx(1); updateMovesTextArea("R");});
+        buttonY1.setOnAction(e -> {turnY(-1); updateMovesTextArea("T");});
+        buttonY2.setOnAction(e -> {turny(1); updateMovesTextArea("D");});
+        buttonZ1.setOnAction(e -> {turnZ(-1); updateMovesTextArea("F");});
+        buttonZ2.setOnAction(e -> {turnz(1); updateMovesTextArea("B");});
+        
+        buttonx1.setOnAction(e -> {turnx(-1); updateMovesTextArea("L'");});
+        buttonx2.setOnAction(e -> {turnX(1); updateMovesTextArea("R'");});
+        buttony1.setOnAction(e -> {turny(-1); updateMovesTextArea("T'");});
+        buttony2.setOnAction(e -> {turnY(1); updateMovesTextArea("D'");});
+        buttonz1.setOnAction(e -> {turnz(-1); updateMovesTextArea("F'");});
+        buttonz2.setOnAction(e -> {turnZ(1); updateMovesTextArea("B'");});
 
-        buttonx1.setOnAction(e -> turnx(-1));
-        buttonx2.setOnAction(e -> turnX(1));
-        buttony1.setOnAction(e -> turny(-1));
-        buttony2.setOnAction(e -> turnY(1));
-        buttonz1.setOnAction(e -> turnz(-1));
-        buttonz2.setOnAction(e -> turnZ(1));
-        mix.setOnAction(e -> mixCube());
+        mix.setOnAction(e -> mixCube(movesTextArea));
+        bReset.setOnAction(e -> resetCube(movesTextArea));
     }
 
-    void resetCube() {
+    void resetCube(TextArea movesTextArea) {
+        movesTextArea.clear();
+        textHistory.delete(0, textHistory.length());
         for (Cubie qb : cube) {
             qb.reset();
         }
     }
-    void mixCube() {
+    void mixCube(TextArea movesTextArea) {
         Timeline timeline = new Timeline();
         for (int i = 0; i < 100; i++) {
             timeline.getKeyFrames().add(new KeyFrame(Duration.millis(i * 100), event -> {
                 int r = (int)(Math.random() * 12) + 1;
+                String move = "";
                 switch(r){
-                    case 1 : turnX(-1);
+                    case 1 : turnX(-1); move = "L";
                         break;
-                    case 2 : turnX(1);
+                    case 2 : turnX(1); move = "R";
                         break;
-                    case 3 : turnx(-1);
+                    case 3 : turnx(-1); move = "L'";
                         break;
-                    case 4 : turnx(1);
+                    case 4 : turnx(1); move = "R'";
                         break;
-                    case 5 : turnY(-1);
+                    case 5 : turnY(-1); move = "T";
                         break;
-                    case 6 : turnY(1);
+                    case 6 : turnY(1); move = "D";
                         break;
-                    case 7 : turny(-1);
+                    case 7 : turny(-1); move = "T'";
                         break;
-                    case 8 : turny(1);
+                    case 8 : turny(1); move = "D'";
                         break;
-                    case 9 : turnZ(-1);
+                    case 9 : turnZ(-1); move = "F";
                         break;
-                    case 10 : turnZ(1);
+                    case 10 : turnZ(1); move = "B";
                         break;
-                    case 11 : turnz(-1);
+                    case 11 : turnz(-1); move = "F'";
                         break;
-                    case 12 : turnz(1);
+                    case 12 : turnz(1); move = "B'";
                         break;
+                    default: break;
                 }
+                textHistory.append(move).append(",  ");
+                movesTextArea.setText(textHistory.toString());
             }));
         }
         timeline.play();
     }
+
+    void updateMovesTextArea(String move) {
+        textHistory.append(move).append(",  ");
+        movesTextArea.setText(textHistory.toString());
+    }
+
     // otáčanie strán podľa X Y Z
     void turnX(int index) {
         for (Cubie qb : cube) {
